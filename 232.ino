df1d1010e33a434a6868c749cc9d5441564beee2
natidim91 <n.dimitrova.uch@gmail.com>
2025-04-24 22:41:22 +0300

    Add files via upload

---
+++ 232.ino
@@ -0,0 +1,65 @@
+#include "arduino_secrets.h"
+
+
+/*
+ * This file is part of 232_noit_MetronMG
+ *
+ * Copyright (C) 2025 Georgi_D. Baran_N.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int trigPin = 3;
+const int echoPin = 2;
+
+void setup() {
+ Serial.begin(9600);
+
+  lcd.init();
+ lcd.backlight();
+
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+
+  float distance = duration * 0.034 / 2;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Distance:");
+  lcd.setCursor(0, 1);
+  lcd.print(distance);
+  lcd.print(" cm");
+
+  
+  Serial.print("Razstoyanie: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);
+}
---
+++ README.txt
@@ -0,0 +1,30 @@
+# MetronMG  
+
+MetronMG is an open-source software for measuring distances in centimeters using ultrasonic sensors. It is designed for microcontrollers such as Arduino and provides accurate real-time distance measurements.  
+
+## Features  
+- Measures distance using ultrasonic sensors  
+- Displays results on an LCD screen  
+- Optimized for real-time measurement  
+- Compatible with Arduino and similar microcontrollers  
+- Open-source and follows GNU coding standards  
+
+## Requirements  
+- Arduino board (e.g., Arduino Uno)  
+- Ultrasonic sensor (e.g., HC-SR04)  
+- LCD display (optional)  
+- C++ compiler and Arduino IDE  
+
+## Вход за достъп до ArduinoCloudAgent
+
+Потребител: noit_ppmg
+Парола: noit_ppmg
+
+Изпълним код: 232
+
+## Забележка:
+Презентацията по възможност да се разгледа в MS 365, заради прехода на слайдовете.
+Линк към сайта, който създадохме:
+https://metronmg.wordpress.com/
+
+
